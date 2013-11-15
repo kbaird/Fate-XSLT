b11502d2c0f600d8685f2bf262e6116030a276fe
Kevin C. Baird <kcbaird@world.oberlin.edu>
2013-11-15 09:54:55 -0800

    Fix validate.sh

--- characters/validate.sh
+++ characters/validate.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 
-# FIXME: Still needs DTD arg
-
-if [ $# -eq 0 ] ; then
-  echo
-  echo "Usage: $(basename $0) some_file(s).xml" >&2
-  echo
-  exit 1
-fi
-
-for file in $@; do
-  xmllint --noout $file
-done
+xmllint --noout --dtdvalid $@
