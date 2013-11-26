0d44a98a4c26484f53ff3ae5ce7c19cb06cdd8c3
Kevin C. Baird <kcbaird@world.oberlin.edu>
2013-11-26 14:21:39 -0800

    Add colors to usage messages

--- characters/validate.sh
+++ characters/validate.sh
@@ -2,7 +2,7 @@
 
 if [ $# -lt 2 ] ; then
   echo
-  echo "Usage: $(basename $0) some_file.dtd some_file(s).xml" >&2
+  echo -e "\e[01;34mUsage\e[0m: \e[01;33m$(basename $0)\e[0m some_file.dtd some_file(s).xml" >&2
   echo
   exit 1
 fi
