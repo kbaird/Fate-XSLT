2a28dfc72c10c7abb4255a4ec964bf1ef28a1bc6
Kevin C. Baird <kcbaird@world.oberlin.edu>
2013-11-28 08:19:30 -0800

    Make set (jastenave or destiny) an arg, defaulting to destiny

--- roll.rb
+++ roll.rb
@@ -10,13 +10,23 @@
 # http://www.sjgames.com/pyramid/sample.html?id=1686
 # http://thealexandrian.net/wordpress/2781/roleplaying-games/dice-of-destiny
 
-#DESCRIPTIONS = %w(environment instinct training equipment)
-DESCRIPTIONS = [
-  'time required',
-  'outside influences',
-  'skill',
-  'style'
-]
+DESCRIPTIONS = {
+
+  # https://twitter.com/lumpleygames/status/403556521579384832
+  jastenave: %w(environment instinct training equipment),
+
+  # http://thealexandrian.net/wordpress/2793/roleplaying-games/dice-of-destiny-part-2-qualities
+  destiny: [
+    'time required',
+    'outside influences',
+# 'knowledge',
+    'skill',
+#'luck',
+    'style',
+#'power',
+#'finesse',
+  ]
+}
 
 def format(results)
   results.inject({}) do |h,pair|
@@ -27,9 +37,9 @@ def format(results)
   end
 end
 
-def roll
+def roll(set)
   get_results = ->(memo,desc) { memo.merge(desc => roll_die) }
-  results = DESCRIPTIONS.inject({}, &get_results)
+  results = DESCRIPTIONS[set].inject({}, &get_results)
   total   = results.map(&:last).inject(:+)
   [total, format(results)]
 end
@@ -38,4 +48,5 @@ def roll_die
   rand(-1..1)
 end
 
-puts roll
+set = ARGV[0] || :destiny
+puts roll(set.to_sym)
