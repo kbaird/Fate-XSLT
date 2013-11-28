a3cc506bf948481fc09f0b764c6f9fc6b11595fa
Kevin C. Baird <kcbaird@world.oberlin.edu>
2013-11-27 23:06:51 -0800

    Add some formatting & use qualities from   http://thealexandrian.net/wordpress/2793/roleplaying-games/dice-of-destiny-part-2-qualities

--- roll.rb
+++ roll.rb
@@ -10,13 +10,28 @@
 # http://www.sjgames.com/pyramid/sample.html?id=1686
 # http://thealexandrian.net/wordpress/2781/roleplaying-games/dice-of-destiny
 
-DESCRIPTIONS = %w(environment instinct training equipment)
+#DESCRIPTIONS = %w(environment instinct training equipment)
+DESCRIPTIONS = [
+  'time required',
+  'outside influences',
+  'skill',
+  'style'
+]
+
+def format(results)
+  results.inject({}) do |h,pair|
+    head, tail = *pair
+    tail = tail < 1 ? tail.to_s : "-#{tail}"
+    new_pair   = {head => tail}
+    h.merge(new_pair)
+  end
+end
 
 def roll
   get_results = ->(memo,desc) { memo.merge(desc => roll_die) }
   results = DESCRIPTIONS.inject({}, &get_results)
   total   = results.map(&:last).inject(:+)
-  [total, results]
+  [total, format(results)]
 end
 
 def roll_die
