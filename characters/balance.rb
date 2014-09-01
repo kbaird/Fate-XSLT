a0e4dd52ad2c5e150c26e7ae3648affbd7f05705
Kevin C. Baird <kcbaird@world.oberlin.edu>
2014-09-01 10:28:29 -0700

    Add balance.rb

---
+++ characters/balance.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env/ruby
+require 'nokogiri'
+
+# Reports on skill build costs for Weird Characters in the
+# Atomic Robo RPG
+
+xml    = File.read(ARGV.first)
+doc    = Nokogiri::XML(xml)
+modes  = doc.xpath('//mode')
+totals = modes.map do |mode|
+  skills = mode.xpath('skill')
+  costs  = skills.map { |sk| sk.attribute('cost').to_s.to_i }
+  total  = costs.inject(0, &:+)
+  puts "#{mode.attribute('name')} costs #{total}"
+  total
+end
+
+puts "#{30 - totals.inject(0, &:+)} pts remaining"
