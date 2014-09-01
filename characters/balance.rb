a0b580738688c2231e75fb14f110974b02bfea6b
Kevin C. Baird <kcbaird@world.oberlin.edu>
2014-09-01 11:05:56 -0700

    Add @add to balance.rb
    The math still isn't perfect, but it's at least better.

--- characters/balance.rb
+++ characters/balance.rb
@@ -10,7 +10,8 @@ modes  = doc.xpath('//mode')
 totals = modes.map do |mode|
   skills = mode.xpath('skill')
   costs  = skills.map { |sk| sk.attribute('cost').to_s.to_i }
-  total  = costs.inject(0, &:+)
+  adds   = skills.map { |sk| sk.attribute('add').to_s.to_i }
+  total  = [costs, adds].flatten.inject(0, &:+)
   puts "#{mode.attribute('name')} costs #{total}"
   total
 end
