6f5d30a1f568d9b46d94e5ae9164b42db1692b35
Kevin C. Baird <kcbaird@world.oberlin.edu>
2014-09-10 15:17:51 -0700

    DRY up balance.rb with extract()

--- characters/balance.rb
+++ characters/balance.rb
@@ -4,13 +4,17 @@ require 'nokogiri'
 # Reports on skill build costs for Weird Characters in the
 # Atomic Robo RPG
 
+def extract(attr)
+  ->(sk) { sk.attribute(attr).to_s.to_i }
+end
+
 xml    = File.read(ARGV.first)
 doc    = Nokogiri::XML(xml)
 modes  = doc.xpath('//mode')
 totals = modes.map do |mode|
   skills = mode.xpath('skill')
-  costs  = skills.map { |sk| sk.attribute('cost').to_s.to_i }
-  adds   = skills.map { |sk| sk.attribute('add').to_s.to_i }
+  costs  = skills.map &extract('cost')
+  adds   = skills.map &extract('add')
   total  = [costs, adds].flatten.inject(0, &:+)
   puts "#{mode.attribute('name')} costs #{total}"
   total
