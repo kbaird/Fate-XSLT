8339852e80909d6db9d7674ec9b9a0b99579d813
Kevin C. Baird <kcbaird@world.oberlin.edu>
2015-03-10 09:08:11 -0700

    Re-publish with SRR icon

--- ePubs/publish_sr.sh
+++ ePubs/publish_sr.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 pandoc --toc --toc-depth=2 --epub-chapter-level=2 -S \
+  --epub-cover-image=/usr/local/share/icons/shadowrun_returns.png \
   -o fate_shadowrun.epub fate_shadowrun.md
+
