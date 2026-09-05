2db287d46fbc5be4e55516056fec6791eac4e3c8
Kevin Baird <kevincbaird@gmail.com>
2026-09-05 16:45:24 -0700

    Fix full_check.sh output

--- full_check.sh
+++ full_check.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+trap 'echo "failed"' ERR
+
 XSPEC_TARBALL=https://github.com/xspec/xspec/archive/refs/tags/v4.0.3.tar.gz
 SAXON_JAR=https://repo1.maven.org/maven2/net/sf/saxon/Saxon-HE/12.5/Saxon-HE-12.5.jar
 XMLRESOLVER_JAR=https://repo1.maven.org/maven2/org/xmlresolver/xmlresolver/6.1.0/xmlresolver-6.1.0.jar
@@ -18,7 +20,7 @@ mkdir -p tools
 [ -f tools/xmlresolver.jar ] || curl -fsSLo tools/xmlresolver.jar $XMLRESOLVER_JAR
 
 # Check character XML validity, as in .github/workflows/xmllint.yml
-find characters -name '*.xml' -print0 | xargs -0 -r xmllint --valid
+find characters -name '*.xml' -print0 | xargs -0 -r xmllint --noout --valid
 
 # Run the XSpec suite, as in .github/workflows/xspec.yml
 export SAXON_CP="$PWD/tools/Saxon-HE.jar:$PWD/tools/xmlresolver.jar"
@@ -26,4 +28,4 @@ for f in tests/xspec/*.xspec; do
   tools/xspec/bin/xspec.sh -j -e "$f"
 done
 
-echo "passed" || echo "failed"
+echo "passed"
